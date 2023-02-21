e5088134e5a40de6e7342b8555665df324c2897f
donnol87 <dolochlainn@dolochl-ltmxe3r.internal.salesforce.com>
2023-02-21 20:01:36 +0000

    added bootstrap and extra calculations for tax etc

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ function calculate() {
     document.getElementById("result").textContent = `10% of ${input} is ${result}.`;
     
     // Show the helpfulness options
-    const helpfulness = document.getElementsByName("helpful");
-    for (let i = 0; i < helpfulness.length; i++) {
-      helpfulness[i].style.display = "inline-block";
-    }
+    document.getElementById("helpfulness").style.display = "block";
+    
+    // Clear the input field
+    document.getElementById("inputField").value = "";
   }
   
   // Function to handle radio button selection
@@ -31,17 +31,40 @@ function calculate() {
       const additionalResult = result * 0.41;
       
       feedback = `Glad to hear it was helpful! 41% of the initial percentage calculated (${result}) is ${additionalResult}.`;
+      
+      // Show the refresh button
+      document.getElementById("refreshButton").style.display = "block";
     } else if (noRadio.checked) {
       feedback = "Sorry to hear that. Please let us know how we can improve.";
     } else {
       return; // Do nothing if no radio button is selected
     }
-    document.getElementById("result").textContent += ` ${feedback}`;
+    document.getElementById("feedback").textContent = feedback;
   }
   
   // Add event listeners for radio button selection
   const helpfulnessRadios = document.getElementsByName("helpful");
   for (let i = 0; i < helpfulnessRadios.length; i++) {
     helpfulnessRadios[i].addEventListener("change", handleHelpfulness);
+  }
+  
+  // Function to refresh the page
+  function refresh() {
+    location.reload();
+  }
+  function calculate() {
+    var inputField = document.getElementById("inputField");
+    var result = document.getElementById("result");
+    var helpfulness = document.getElementById("helpfulness");
+    var loadingSpinner = document.getElementById("loadingSpinner");
+  
+    loadingSpinner.style.display = "block";
+    setTimeout(function() {
+      var percent = inputField.value * 0.1;
+      result.innerText = "10% of " + inputField.value + " is " + percent.toFixed(2);
+  
+      helpfulness.style.display = "block";
+      loadingSpinner.style.display = "none";
+    }, 2000);
   }
   
