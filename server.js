8597c2d5c9c32a7900b3e8b00490934dc66b43b0
donnol87 <dolochlainn@dolochl-ltmxe3r.internal.salesforce.com>
2023-02-21 19:39:14 +0000

    updated html & js with chatGPT code to calculate commission and income tax

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-//variables for each item called out by an id in the HTML page
-let ourForm = document.getElementById("ourForm")
-let ourField = document.getElementById("ourField")
-let ourList = document.getElementById("ourList")
-
-// create an event on submit and make sure the default isn't set
-ourForm.addEventListener("submit", (e) =>{
-    e.preventDefault()
-    createItem(ourField.value)
-})
-
-//section of code to create a list item when they are put into the input element
-function createItem(x){
-    let ourHTML = `<li>${x}<button onclick="deleteItem(this)">Delete</button></li>`
-    ourList.insertAdjacentHTML("beforeend", ourHTML)
-    ourField.value = ""
-    ourField.focus()
-}
-
-// section of code to delete each item on delete button clicj
-function deleteItem(elementToDelete){
-    elementToDelete.parentElement.remove()
-}
+function calculate() {
+    // Get the value of the input field
+    const input = document.getElementById("inputField").value;
+    
+    // Calculate 10% of the input
+    const result = input * 0.1;
+    
+    // Display the result
+    document.getElementById("result").textContent = `10% of ${input} is ${result}.`;
+    
+    // Show the helpfulness options
+    const helpfulness = document.getElementsByName("helpful");
+    for (let i = 0; i < helpfulness.length; i++) {
+      helpfulness[i].style.display = "inline-block";
+    }
+  }
+  
+  // Function to handle radio button selection
+  function handleHelpfulness() {
+    const yesRadio = document.getElementById("yes");
+    const noRadio = document.getElementById("no");
+    let feedback = "";
+    if (yesRadio.checked) {
+      // Get the value of the input field
+      const input = document.getElementById("inputField").value;
+  
+      // Calculate 10% of the input
+      const result = input * 0.1;
+  
+      // Calculate 41% of the initial percentage calculated
+      const additionalResult = result * 0.41;
+      
+      feedback = `Glad to hear it was helpful! 41% of the initial percentage calculated (${result}) is ${additionalResult}.`;
+    } else if (noRadio.checked) {
+      feedback = "Sorry to hear that. Please let us know how we can improve.";
+    } else {
+      return; // Do nothing if no radio button is selected
+    }
+    document.getElementById("result").textContent += ` ${feedback}`;
+  }
+  
+  // Add event listeners for radio button selection
+  const helpfulnessRadios = document.getElementsByName("helpful");
+  for (let i = 0; i < helpfulnessRadios.length; i++) {
+    helpfulnessRadios[i].addEventListener("change", handleHelpfulness);
+  }
+  
