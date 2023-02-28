a1cfaf57d925971cf505299d11198419d897c4b4
donnol87 <dolochlainn@dolochl-ltmxe3r.internal.salesforce.com>
2023-02-28 13:43:49 +0000

    updated server.js file for ios users to only add in numbers

--- server.js
+++ server.js
@@ -75,4 +75,25 @@ function calculate() {
   }
   "use strict";
 
+  function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+      textbox.addEventListener(event, function() {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        }
+      });
+    });
+  }
+  
+  // Restrict input to only numbers
+  setInputFilter(document.getElementById("inputField"), function(value) {
+    return /^\d*$/.test(value);
+  });
+  
+
  
